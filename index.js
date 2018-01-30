54d71d23897022fa12b843ce33e534f2d63e47a3
Stephen Cook <stephen@stephencookdev.co.uk>
2018-01-30 19:51:27 +0000

    Add prettier for formatting

--- index.js
+++ index.js
@@ -1,114 +1,128 @@
 const fs = require("fs");
 const { WrappedPlugin } = require("./WrappedPlugin");
 const { getModuleName, getLoaderNames } = require("./utils");
-const { getHumanOutput, getMiscOutput, getPluginsOutput, getLoadersOutput } = require("./output");
+const {
+  getHumanOutput,
+  getMiscOutput,
+  getPluginsOutput,
+  getLoadersOutput,
+} = require("./output");
 
 module.exports = class SpeedMeasurePlugin {
-	constructor(options) {
-		this.options = options || {};
+  constructor(options) {
+    this.options = options || {};
 
-		this.timeEventData = {};
+    this.timeEventData = {};
 
-		this.getOutput = this.getOutput.bind(this);
-		this.addTimeEvent = this.addTimeEvent.bind(this);
-		this.apply = this.apply.bind(this);
-	}
+    this.getOutput = this.getOutput.bind(this);
+    this.addTimeEvent = this.addTimeEvent.bind(this);
+    this.apply = this.apply.bind(this);
+  }
 
-	static wrapPlugins(plugins, options) {
-		const smp = new SpeedMeasurePlugin(options);
+  static wrapPlugins(plugins, options) {
+    const smp = new SpeedMeasurePlugin(options);
 
-		if(Array.isArray(plugins)) {
-			let i = 1;
-			plugins = plugins.reduce((acc, p) => ({
-				...acc,
-				["plugin " + (i++)]: p,
-			}));
-		}
-		plugins = plugins || {};
+    if (Array.isArray(plugins)) {
+      let i = 1;
+      plugins = plugins.reduce((acc, p) => ({
+        ...acc,
+        ["plugin " + i++]: p,
+      }));
+    }
+    plugins = plugins || {};
 
-		const wrappedPlugins = Object
-			.keys(plugins)
-			.map(pluginName => new WrappedPlugin(plugins[pluginName], pluginName, smp));
+    const wrappedPlugins = Object.keys(plugins).map(
+      pluginName => new WrappedPlugin(plugins[pluginName], pluginName, smp)
+    );
 
-		return wrappedPlugins.concat(smp);
-	}
+    return wrappedPlugins.concat(smp);
+  }
 
-	getOutput() {
-		const outputObj = {};
-		if(this.timeEventData.misc)
-			outputObj.misc = getMiscOutput(this.timeEventData.misc);
-		if(this.timeEventData.plugins)
-			outputObj.plugins = getPluginsOutput(this.timeEventData.plugins);
-		if(this.timeEventData.loaders)
-			outputObj.loaders = getLoadersOutput(this.timeEventData.loaders);
+  getOutput() {
+    const outputObj = {};
+    if (this.timeEventData.misc)
+      outputObj.misc = getMiscOutput(this.timeEventData.misc);
+    if (this.timeEventData.plugins)
+      outputObj.plugins = getPluginsOutput(this.timeEventData.plugins);
+    if (this.timeEventData.loaders)
+      outputObj.loaders = getLoadersOutput(this.timeEventData.loaders);
 
-		return this.options.outputFormat === "human"
-			? getHumanOutput(outputObj)
-			: JSON.stringify(outputObj, null, 2);
-	}
+    return this.options.outputFormat === "human"
+      ? getHumanOutput(outputObj)
+      : JSON.stringify(outputObj, null, 2);
+  }
 
-	addTimeEvent(category, event, eventType, data = {}) {
-		const tED = this.timeEventData;
-		if(!tED[category]) tED[category] = {};
-		if(!tED[category][event]) tED[category][event] = [];
-		const eventList = tED[category][event];
-		const curTime = new Date().getTime();
+  addTimeEvent(category, event, eventType, data = {}) {
+    const tED = this.timeEventData;
+    if (!tED[category]) tED[category] = {};
+    if (!tED[category][event]) tED[category][event] = [];
+    const eventList = tED[category][event];
+    const curTime = new Date().getTime();
 
-		if(eventType === "start") {
-			eventList.push({
-				start: curTime,
-				...data,
-			});
-		} else if(eventType === "end") {
-			const matchingEvent = eventList.find(e => !e.end && (e.id === data.id || e.name === data.name));
-			const eventToModify = matchingEvent || eventList.find(e => !e.end);
-			if(!eventToModify) {
-				console.log("Could not find a matching event to end", category, event, data);
-				throw new Error("No matching event!");
-			}
+    if (eventType === "start") {
+      eventList.push({
+        start: curTime,
+        ...data,
+      });
+    } else if (eventType === "end") {
+      const matchingEvent = eventList.find(
+        e => !e.end && (e.id === data.id || e.name === data.name)
+      );
+      const eventToModify = matchingEvent || eventList.find(e => !e.end);
+      if (!eventToModify) {
+        console.log(
+          "Could not find a matching event to end",
+          category,
+          event,
+          data
+        );
+        throw new Error("No matching event!");
+      }
 
-			eventToModify.end = curTime;
-		}
-	}
+      eventToModify.end = curTime;
+    }
+  }
 
-	apply(compiler) {
-		compiler.plugin("compile", () => {
-			this.addTimeEvent("misc", "compile", "start", { watch: false });
-		});
-		compiler.plugin("done", () => {
-			this.addTimeEvent("misc", "compile", "end");
+  apply(compiler) {
+    compiler.plugin("compile", () => {
+      this.addTimeEvent("misc", "compile", "start", { watch: false });
+    });
+    compiler.plugin("done", () => {
+      this.addTimeEvent("misc", "compile", "end");
 
-			const output = this.getOutput();
-			if(this.options.outputTarget) {
-				const writeMethod = fs.existsSync(this.options.outputTarget) ? fs.appendFile : fs.writeFile;
-				writeMethod(this.options.outputTarget, output + "\n", err => {
-					if(err) throw err;
-					console.log("Outputted timing info to " + this.options.outputTarget);
-				});
-			} else {
-				console.log(output);
-			}
+      const output = this.getOutput();
+      if (this.options.outputTarget) {
+        const writeMethod = fs.existsSync(this.options.outputTarget)
+          ? fs.appendFile
+          : fs.writeFile;
+        writeMethod(this.options.outputTarget, output + "\n", err => {
+          if (err) throw err;
+          console.log("Outputted timing info to " + this.options.outputTarget);
+        });
+      } else {
+        console.log(output);
+      }
 
-			this.timeEventData = {};
-		});
+      this.timeEventData = {};
+    });
 
-		compiler.plugin("compilation", compilation => {
-			compilation.plugin("build-module", module => {
-				const name = getModuleName(module);
-				if(name) {
-					this.addTimeEvent("loaders", "build", "start", {
-						name,
-						loaders: getLoaderNames(module.loaders),
-					});
-				}
-			});
+    compiler.plugin("compilation", compilation => {
+      compilation.plugin("build-module", module => {
+        const name = getModuleName(module);
+        if (name) {
+          this.addTimeEvent("loaders", "build", "start", {
+            name,
+            loaders: getLoaderNames(module.loaders),
+          });
+        }
+      });
 
-			compilation.plugin("succeed-module", module => {
-				const name = getModuleName(module);
-				if(name) {
-					this.addTimeEvent("loaders", "build", "end", { name });
-				}
-			});
-		});
-	}
-}
+      compilation.plugin("succeed-module", module => {
+        const name = getModuleName(module);
+        if (name) {
+          this.addTimeEvent("loaders", "build", "end", { name });
+        }
+      });
+    });
+  }
+};
--- utils.js
+++ utils.js
@@ -1,73 +1,84 @@
-const isEqual = (x, y) => Array.isArray(x)
-	? Array.isArray(y) && x.every(xi => y.includes(xi)) && y.every(yi => x.includes(yi))
-	: x === y;
+const isEqual = (x, y) =>
+  Array.isArray(x)
+    ? Array.isArray(y) &&
+      x.every(xi => y.includes(xi)) &&
+      y.every(yi => x.includes(yi))
+    : x === y;
 
 const mergeRanges = rangeList => {
-	const mergedQueue = [];
-	const inputQueue = [...rangeList];
-	while(inputQueue.length) {
-		const cur = inputQueue.pop();
-		const overlapIndex = mergedQueue.findIndex(item =>
-			(item.start >= cur.start && item.start <= cur.end) ||
-			(item.end >= cur.start && item.end <= cur.end)
-		);
+  const mergedQueue = [];
+  const inputQueue = [...rangeList];
+  while (inputQueue.length) {
+    const cur = inputQueue.pop();
+    const overlapIndex = mergedQueue.findIndex(
+      item =>
+        (item.start >= cur.start && item.start <= cur.end) ||
+        (item.end >= cur.start && item.end <= cur.end)
+    );
 
-		if(overlapIndex === -1) {
-			mergedQueue.push(cur);
-		} else {
-			const toMerge = mergedQueue.splice(overlapIndex, 1)[0];
-			inputQueue.push({
-				start: Math.min(cur.start, cur.end, toMerge.start, toMerge.end),
-				end: Math.max(cur.start, cur.end, toMerge.start, toMerge.end),
-			});
-		}
-	};
+    if (overlapIndex === -1) {
+      mergedQueue.push(cur);
+    } else {
+      const toMerge = mergedQueue.splice(overlapIndex, 1)[0];
+      inputQueue.push({
+        start: Math.min(cur.start, cur.end, toMerge.start, toMerge.end),
+        end: Math.max(cur.start, cur.end, toMerge.start, toMerge.end),
+      });
+    }
+  }
 
-	return mergedQueue;
+  return mergedQueue;
 };
 
 const sqr = x => x * x;
 const mean = xs => xs.reduce((acc, x) => acc + x, 0) / xs.length;
 const median = xs => xs.sort()[Math.floor(xs.length / 2)];
-const variance = (xs, mean) => xs.reduce((acc, x) => acc + sqr(x - mean), 0) / (xs.length - 1);
-const range = xs => xs.reduce((acc, x) => ({
-	start: Math.min(x, acc.start),
-	end: Math.max(x, acc.end),
-}), { start: Number.POSITIVE_INFINITY, end: Number.NEGATIVE_INFINITY });
+const variance = (xs, mean) =>
+  xs.reduce((acc, x) => acc + sqr(x - mean), 0) / (xs.length - 1);
+const range = xs =>
+  xs.reduce(
+    (acc, x) => ({
+      start: Math.min(x, acc.start),
+      end: Math.max(x, acc.end),
+    }),
+    { start: Number.POSITIVE_INFINITY, end: Number.NEGATIVE_INFINITY }
+  );
 
 module.exports.getModuleName = module => module.userRequest;
 
-module.exports.getLoaderNames = loaders => loaders.length
-	? loaders
-		.map(l => l.loader)
-		.map(l => l.replace(/^.*\/node_modules\/([^\/]+).*$/, (_, m) => m))
-	: ["(none)"];
+module.exports.getLoaderNames = loaders =>
+  loaders.length
+    ? loaders
+        .map(l => l.loader)
+        .map(l => l.replace(/^.*\/node_modules\/([^\/]+).*$/, (_, m) => m))
+    : ["(none)"];
 
 module.exports.groupBy = (key, arr) => {
-	const groups = [];
-	arr.forEach(arrItem => {
-		const groupItem = groups.find(poss => isEqual(poss[0][key], arrItem[key]));
-		if(groupItem) groupItem.push(arrItem);
-		else groups.push([arrItem]);
-	});
+  const groups = [];
+  arr.forEach(arrItem => {
+    const groupItem = groups.find(poss => isEqual(poss[0][key], arrItem[key]));
+    if (groupItem) groupItem.push(arrItem);
+    else groups.push([arrItem]);
+  });
 
-	return groups;
+  return groups;
 };
 
 module.exports.getAverages = group => {
-	const durationList = group.map(cur => cur.end - cur.start);
+  const durationList = group.map(cur => cur.end - cur.start);
 
-	const averages = {};
-	averages.dataPoints = group.length;
-	averages.median = median(durationList);
-	averages.mean = Math.round(mean(durationList));
-	averages.range = range(durationList);
-	if(group.length > 1) averages.variance = Math.round(variance(durationList, averages.mean));
+  const averages = {};
+  averages.dataPoints = group.length;
+  averages.median = median(durationList);
+  averages.mean = Math.round(mean(durationList));
+  averages.range = range(durationList);
+  if (group.length > 1)
+    averages.variance = Math.round(variance(durationList, averages.mean));
 
-	return averages;
+  return averages;
 };
 
 module.exports.getTotalActiveTime = group => {
-	const mergedRanges = mergeRanges(group);
-	return mergedRanges.reduce((acc, range) => acc + range.end - range.start, 0);
+  const mergedRanges = mergeRanges(group);
+  return mergedRanges.reduce((acc, range) => acc + range.end - range.start, 0);
 };
